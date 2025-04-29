b08a467dcec2613e63666ca9b00c31e78b986cd0
Ameenabibi123 <ameenamahammadanavar8@gmail.com>
2025-04-29 08:57:30 +0530

    master

--- Employee.java
+++ Employee.java
@@ -4,13 +4,15 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String location;
 
 //constructor
-public Employee(String name, int id, double salary){
+public Employee(String name, int id, double salary ,String location){
 
     this.name =name;
     this.id =id;
     this.salary =salary;
+    this.location = location;
 }
 
     //method to display employee details
@@ -18,12 +20,16 @@ public Employee(String name, int id, double salary){
         System.out.println("Employee Id" +id);
         System.out.println("Employee Name" +name);
         System.out.println("salary" +salary);
+        System.out.println("location" +location);
+
     }
 
     //main methos
     public static void main(String[] args) {
         //creating an object of employee
-        Employee emp1 = new Employee("Ameena",101,50000);
+        Employee emp1 = new Employee("Ameena",101,50000,"hubli");
+
         emp1.displayInfo();
+
     }
 }
